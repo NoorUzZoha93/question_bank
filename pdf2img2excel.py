6901c002db0b4a3b20866a27114b11d9c713c292
NoorUzZoha93 <noorzoha786@gmail.com>
2025-01-05 04:59:18 +0300

    Update pdf2img2excel.py

--- pdf2img2excel.py
+++ pdf2img2excel.py
@@ -9,6 +9,10 @@ data = []
 for page in range(3, len(pages)):
     question, explanation, options1, options2, options3, options4,correct_answer  = '', '', '', '', '', '', ''
     parsing_question = False
+    parsing_option1 = False
+    parsing_option2 = False
+    parsing_option3 = False
+    parsing_option4 = False
     parsing_explanation = False
     try:
         page_text = pytesseract.image_to_string(pages[page])
@@ -19,35 +23,75 @@ for page in range(3, len(pages)):
                 question += line
                 question = question.split("Question:")[-1]
                 parsing_question =True
+                parsing_option1 = False
+                parsing_option2 = False
+                parsing_option3 = False
+                parsing_option4 = False
                 parsing_explanation = False
             elif line.startswith('Option 1:'):  # option line
                 options1 += line
                 options1 = options1.split('Option 1:')[-1]
                 parsing_question = False
+                parsing_option1 = True
+                parsing_option2 = False
+                parsing_option3 = False
+                parsing_option4 = False
+                parsing_explanation = False
             elif line.startswith('Option 2:'):  # option line
                 options2 += line
                 options2 = options2.split('Option 2:')[-1]
                 parsing_question = False
+                parsing_option1 = False
+                parsing_option2 = True
+                parsing_option3 = False
+                parsing_option4 = False
+                parsing_explanation = False
             elif line.startswith('Option 3:'):  # option line
                 options3 += line
                 options3 = options3.split('Option 3:')[-1]
                 parsing_question = False
+                parsing_option1 = False
+                parsing_option2 = False
+                parsing_option3 = True
+                parsing_option4 = False
+                parsing_explanation = False
             elif line.startswith('Option 4:'):  # option line
                 options4 += line
                 options4 = options4.split('Option 4:')[-1]
                 parsing_question = False
+                parsing_option1 = False
+                parsing_option2 = False
+                parsing_option3 = False
+                parsing_option4 = True
+                parsing_explanation = False
             elif line.startswith('Correct Response'):  # correct Response line
                 correct_answer += line
                 correct_answer = correct_answer.split('Correct Response:')[-1]
-                parsing_question = False
+                # parsing_question = False
             elif line.startswith('Explanation:'):  # Explanation line
                 explanation += line
-                parsing_explanation =True
-                parsing_question = False
+                parsing_question =False
+                parsing_option1 = False
+                parsing_option2 = False
+                parsing_option3 = False
+                parsing_option4 = False
+                parsing_explanation = True
             else:
                 if parsing_question:
                     question += ' '+line
                     question = question.split("Question:")[-1]
+                elif parsing_option1:
+                    options1 += ' '+line
+                    options1 = options1.split("Option 1:")[-1]
+                elif parsing_option2:
+                    options2 += ' ' + line
+                    options2 = options2.split("Option 2:")[-1]
+                elif parsing_option3:
+                    options3 += ' ' + line
+                    options3 = options3.split("Option 3:")[-1]
+                elif parsing_option4:
+                    options4 += ' ' + line
+                    options4 = options4.split("Option 4:")[-1]
                 elif parsing_explanation:
                     explanation += ' '+line
                     explanation = explanation.split("Explanation:")[-1]
@@ -58,6 +102,6 @@ for page in range(3, len(pages)):
          'Correct_answer': correct_answer, 'Explanation': explanation})
 try:
     df = pd.DataFrame(data)
-    df.to_excel('extracted_data1.xlsx')
+    df.to_excel('Excel_Python_Question_Bank.xlsx')
 except Exception as e:
     print(f'{str(e)}')
